4e23d024239f0ef3f792871ceea73426d2d9504f
Hafez92 <abdul.hafez@kcl.ac.uk>
2016-09-08 18:22:59 +0100

    Update my_first_tour.js

--- .idea/hopscotch/my_first_tour.js
+++ .idea/hopscotch/my_first_tour.js
@@ -1,1 +1,13 @@
-
+  {
+      id: "hello-hopscotch",
+      steps: [
+        {
+          target: "",
+          title: "Creating a group!",
+          content: "Hey there, this is a product tour of FreeBusy, our first feature of the tour is our group scheduling for team meeting's ",
+          placement: "bottom",
+        },
+              ],
+      showPrevButton: true,
+      scrollTopMargin: 100
+    }
